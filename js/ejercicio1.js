6b9e499f45c1d7262de6553e150f1a517bec6960
NESTOR\nesto <nestorcostaorts@gmail.com>
2022-11-14 15:47:24 +0100

    Commit JavaScript

---
+++ js/ejercicio1.js
@@ -0,0 +1,37 @@
+console.info("Primer Script");
+
+console.warn("Error en el codigo");
+
+console.error("Error de enlace");
+
+console.log("Mensaje de tipo Log");
+
+console.time();
+
+console.timeEnd();
+
+console.log("Prove", 1, {}, [14,50,69]);
+
+function Sum(){
+    var numero1=2;
+
+    var numero2=34;
+
+    var resultado = numero1+numero2;
+
+    return resultado;
+}
+
+console.log(resultado);
+
+function Restar(){
+    var numero1=50;
+
+    var numero2=34;
+
+    var resultado = numero1-numero2;
+
+    return resultado2;
+}
+
+console.log(resultado2);
