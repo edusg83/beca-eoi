7ea2c02b177752bd2fabcbd74c88b27fac95a7a4
NESTOR\nesto <nestorcostaorts@gmail.com>
2022-11-14 15:48:34 +0100

    Commit Cambios

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -12,26 +12,21 @@ console.timeEnd();
 
 console.log("Prove", 1, {}, [14,50,69]);
 
-function Sum(){
+
     var numero1=2;
 
     var numero2=34;
 
     var resultado = numero1+numero2;
 
-    return resultado;
-}
 
 console.log(resultado);
 
-function Restar(){
+
     var numero1=50;
 
     var numero2=34;
 
-    var resultado = numero1-numero2;
-
-    return resultado2;
-}
+    var resultado2 = numero1-numero2;
 
 console.log(resultado2);
