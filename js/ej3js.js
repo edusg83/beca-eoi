1c81ad091c0aedbd72d1618136c0db82bff481e1
David López <DAVID@PC.(none)>
2022-11-14 16:51:41 +0100

    js3

--- js/ej3js.js
+++ js/ej3js.js
@@ -1,5 +1,7 @@
 var cadena1 = "CADENA1";
+var cadena2 = "CADENA2";
 console.log(cadena1.indexOf("ENA"));
 console.log(cadena1.charAt(cadena1.length-1));
 console.log(cadena1.toLowerCase());
 console.log(cadena1.replace("DE","ME"));
+console.log(cadena1+cadena2);
