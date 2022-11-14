ecca49c524a90da48943c803c28ec7e994c39338
David López <DAVID@PC.(none)>
2022-11-14 20:33:16 +0100

    js8

--- js/ej8js.js
+++ js/ej8js.js
@@ -1,8 +1,10 @@
 function suma (param1,param2){
-    var resultado = param1+param2
+    var resultado = param1+param2;
+    document.getElementById("resultado").innerHTML=(param1+param2);
     return console.log(resultado);
 }
 
 (function (param1,param2){
-    suma(param1,param2);
+    var resultado = param1+param2;
+    return console.log(resultado)
 }(12,12))
