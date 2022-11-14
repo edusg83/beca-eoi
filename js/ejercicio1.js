f2571b8359df53951ebc62da940ca3a4dd028e39
Javi <mail>
2022-11-14 15:45:58 +0100

    commit

---
+++ js/ejercicio1.js
@@ -0,0 +1,13 @@
+console.log("MENSAJE DE INFO");
+console.warn("MENSAJE DE WARNING");
+console.error("MENSAJE DE ERROR");
+console.log("MENSAJE DE LOG");
+console.time();
+console.timeEnd();
+
+var numero1=2
+var numero2=34
+console.log(numero1+numero2)
+console.log(numero1-numero2)
+console.log({})
+console.log([numero1,numero2])
