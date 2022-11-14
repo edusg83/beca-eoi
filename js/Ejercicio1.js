09c99a5011918ce33a3a6df8181f960d4531cba9
eseverag <pctpct96@gmail.com>
2022-11-14 15:56:29 +0100

    EjercicioJS1

---
+++ js/Ejercicio1.js
@@ -0,0 +1,12 @@
+console.info("Mensaje de info");
+console.warn("Mensaje de warning");
+console.error("Mensaje de error");
+console.log("Mensaje de log");
+console.time();
+console.timeEnd();
+
+var numero1=2;
+var numero2=34;
+
+console.info(numero1+numero2);
+console.info(numero1-numero2);
