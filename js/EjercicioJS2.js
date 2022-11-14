8ea0a2d7b3644f56cc73788f99661d3b0af9e598
eseverag <pctpct96@gmail.com>
2022-11-14 16:57:30 +0100

    EjercicioJS3

--- js/EjercicioJS2.js
+++ js/EjercicioJS2.js
@@ -1,7 +1,6 @@
-var alto= 12;
-var ancho= 12.203004;
-var altoPorAncho= alto*ancho;
-var altoPorAncho2= altoPorAncho.toFixed(2);
+var alto= 12,
+    ancho= 12.203004,
+    altoPorAncho= alto*ancho;
 
 console.log(altoPorAncho);
-console.log(altoPorAncho2);
+console.log(altoPorAncho.toFixed(2));
