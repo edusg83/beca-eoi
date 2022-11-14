d6fdbef2fb46f45f0cd2a89a4eff0f8778ae300f
edusg83 <edusg83@gmail.com>
2022-11-14 16:16:57 +0100

    javascript

---
+++ js/ejercicios.js
@@ -0,0 +1,9 @@
+var numero1=1;
+
+var miVariable, miVariable2=3, miVariable3="un valor";
+
+console.log(typeof(miVariable));
+console.log(typeof(miVariable2));
+
+var variable1 = new Number(16);
+console.log(variable1.valueOf());
