3e658345dd1cd1f1f57e1c9f666990ef68785619
Manuel <manuel.molina.munoz99@gmail.com>
2022-11-14 17:56:05 +0100

    ej5

---
+++ js/ejercicio5.js
@@ -0,0 +1,27 @@
+var array1 = [1, 2, 3, 4];
+
+for(var i = 0; i<array1.length + 1; i++){
+    switch(i){
+        case 1:
+            console.log("UNO")
+            break
+        case 2:
+            console.log("DOS")
+            break
+        case 3:
+            console.log("TRES")
+            break
+        case 4:
+            console.log("CUATRO")
+            break
+    }
+}
+
+array1.push(3);
+console.log(array1)
+
+for(var i = 0; i<3; i++){
+    array1.pop();
+}
+console.log(array1)
+
