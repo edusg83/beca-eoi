d9eaf23a3c3511858d9ef441ccc2215347dc62e5
Manuel <manuel.molina.munoz99@gmail.com>
2022-11-14 16:53:22 +0100

    ej 3

--- js/ejercicio2.js
+++
@@ -1,7 +0,0 @@
-
-var alto = new Number(12);
-var ancho = new Number(12.0365);
-var altoporancho = new Number(alto*ancho);
-
-
-console.log(altoporancho.toPrecision(2))
---
+++ js/ejercicio3.js
@@ -0,0 +1,18 @@
+
+var alto = new Number(12);
+var ancho = new Number(12.0365);
+var altoporancho = new Number(alto*ancho);
+
+
+
+var str1 = "CADENA1";
+var str2 = "CADENA2";
+
+
+console.log(str1.indexOf("ENA"));
+console.log(str1.charAt(str1.length-1));
+console.log(str1.toLowerCase());
+console.log(str1.replace("DE", "ME"))
+console.log(str1.concat(str2))
+
+console.log(altoporancho.toFixed(2))
