49c82c990bbe8c38129f86d4431272e08eaf87ff
Javi <mail>
2022-11-14 19:06:28 +0100

    commit

---
+++ js/ejercicio6.js
@@ -0,0 +1,14 @@
+let array1 = [1,2,3,4];
+for(var i in array1){
+    switch(array1[i]){
+        case 1: console.log(array1[i]);
+        break;
+        case 2: console.log(array1[i]);
+        break;
+        case 3: console.log(array1[i]);
+        break;
+        case 4: console.log(array1[i]);
+        break;
+    }
+}
+
