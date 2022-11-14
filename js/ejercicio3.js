48d4680d37cac80374e53fa80f5bb941e881daa1
NESTOR\nesto <nestorcostaorts@gmail.com>
2022-11-14 16:56:44 +0100

    Commit Corregido

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -2,7 +2,7 @@ var cadena1 = "CADENA1";
 
 console.log(cadena1.indexOf("ENA",2));
 
-console.log(cadena1.lastIndexOf(1));
+console.log(cadena1.charAt(cadena1.length - 1));
 
 console.log(cadena1.toLowerCase());
 
