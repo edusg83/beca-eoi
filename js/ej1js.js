43bac07fd1003fa17c711bf8a603ff4d00e05176
David López <DAVID@PC.(none)>
2022-11-14 15:35:55 +0100

    JS y distribucion

--- js/ej1js.js
+++ js/ej1js.js
@@ -2,3 +2,7 @@ console.log("prueba",1,{},[1,2,3]);
 console.info("informacion");
 console.warn("Warning");
 console.error("Error");
+console.dir(1+2);
+console.dir(2-2);
+console.dir({objeto1: 1});
+console.dir([{objeto1: 1},{objeto2: 2},{objeto3: 3}]);
