cc3ec0bcb8704551c821a69b203b265a00f47ecc
Manuel <manuel.molina.munoz99@gmail.com>
2022-11-14 15:42:17 +0100

    ej1 js

---
+++ js/ejercicio1.js
@@ -0,0 +1,16 @@
+console.info("MENSAJE DE INFO");
+console.warn("MENSAJE DE WARNING");
+console.error("MENSAJE DE ERROR");
+console.log("MENSAJE DE LOG");
+console.time();
+
+
+var numero1 = 2;
+var numero2 = 34;
+
+var suma = numero1+numero2;
+var resta = numero1-numero2;
+
+console.log(suma, resta, {})
+Array1 = [{}, {}, {}];
+console.log(Array1);
