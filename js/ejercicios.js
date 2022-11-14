7e9acf4e8dc6e423f5e5dc488312e7d1d2f20db3
edusg83 <edusg83@gmail.com>
2022-11-14 19:48:11 +0100

    js

--- js/ejercicios.js
+++ js/ejercicios.js
@@ -23,3 +23,27 @@ console.log(typeof(sema)===undefined);
 
 
 
+let numeroUno=1;
+let resultado;
+
+numeroUno+=2;
+numeroUno=numeroUno+2;
+
+console.log(numeroUno);
+
+var verdadero = ("1"==true);
+var falso = ("1"===true);
+
+console.log(verdadero);
+console.log(falso);
+
+var a="Er";
+a += "ic";
+var b=a>"John Lennon" || a + " Clapton";
+a=b;
+var c = !(a>b || a != b) && a!="Eric";
+
+console.log("a: "+a);
+console.log("b: "+b);
+console.log("c: "+c);
+
