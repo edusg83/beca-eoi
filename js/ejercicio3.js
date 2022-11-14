624f37c2e80375be05cd2132b293ee03bb5e662a
NESTOR\nesto <nestorcostaorts@gmail.com>
2022-11-14 16:53:52 +0100

    Commit JS 3

---
+++ js/ejercicio3.js
@@ -0,0 +1,17 @@
+var cadena1 = "CADENA1";
+
+console.log(cadena1.indexOf("ENA",2));
+
+console.log(cadena1.lastIndexOf(1));
+
+console.log(cadena1.toLowerCase());
+
+cadena = cadena1.replace("DE","ME");
+
+console.log(cadena);
+
+var cadena2 = "CADENA2";
+
+cf = cadena2 + cadena1;
+
+console.log(cf);
