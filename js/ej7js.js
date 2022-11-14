a6158a273a9262649fa3c5254678b313761cec1c
David López <DAVID@PC.(none)>
2022-11-14 20:10:33 +0100

    js8

--- js/ej7js.js
+++ js/ej7js.js
@@ -7,3 +7,8 @@ console.log(pieza1['peso'] === pieza2['peso']);
 
 var calculo = (pieza1['peso'] == '20') ? pieza1['peso']+Number(pieza2['peso']) : pieza1['peso']*Number(pieza1['peso']);
 console.log(calculo);
+
+function Test(op1,op2){
+
+    return null;
+}
---
+++ js/ej8js.js
@@ -0,0 +1,9 @@
+function suma (param1,param2){
+    var resultado = param1+param2
+    return console.log(resultado);
+}
+
+(function suma (param1,param2){
+    var resultado = param1+param2
+    return console.log(resultado);
+}(5,5))
