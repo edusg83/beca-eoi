24437df9a5e2126d5d21bfbf21227a42d468279c
Loren0723 <lorenelx@gmail.com>
2022-11-14 16:15:56 +0100

    commit

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -5,15 +5,16 @@ var objeto2 = "rojo";
 var objeto3 = "azul";
 var objeto4 = "verde";
 
+console.time("Time");
+
 console.info("MENSAJE DE INFO");
 console.warn("MENSAJE DE WARNING");
 console.error("MENSAJE DE ERROR");
 console.log("MENSAJE DE LOG");
 
-console.time("Time");
 console.timeEnd("Time");
 
 console.log(numero1 + numero2);
 console.log(numero1 - numero2);
 console.log({objeto1, objeto2, objeto3, objeto4});
-console.log("objeto", 1, {}, [objeto2, objeto3, objeto4]);
+console.log("objeto", 1, [objeto2, objeto3, objeto4]);
