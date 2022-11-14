4fcdccc4d3ed8e97186963d6e689be354ce7d745
NESTOR\nesto <nestorcostaorts@gmail.com>
2022-11-14 20:29:29 +0100

    Commit js8

---
+++ js/ejercicio8.js
@@ -0,0 +1,19 @@
+var param1 = 12;
+
+var param2 = 12;
+
+var result = param1 + param2;
+
+function Sum(param1, param2) {
+    alert(param1 + param2);
+    document.getElementById("resultado").innerHTML=param1 + param2;
+    return console.log(result);  
+}
+
+Sum(12,12);
+
+console.log(result);
+
+(function(param1, param2) {
+    console.log(param1+param2);
+}(12,12));
