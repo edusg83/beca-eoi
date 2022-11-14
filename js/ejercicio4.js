8bf1a9c629f03cf7d85f45ce41769e962a276452
NESTOR\nesto <nestorcostaorts@gmail.com>
2022-11-14 17:39:18 +0100

    Commit js4

---
+++ js/ejercicio4.js
@@ -0,0 +1,19 @@
+var cadena1 = "CADENA1";
+
+var cadena2;
+
+console.log(typeof(cadena1));
+console.log(typeof(cadena2));
+
+if(cadena2 == undefined){
+    console.info("SIN DEFINIR");
+}
+
+let cliente = {
+    nombre:"PEPITO",
+    tel:"656666666"
+};
+
+cliente.direccion='C/ Salud,21';
+
+console.log(cliente);
