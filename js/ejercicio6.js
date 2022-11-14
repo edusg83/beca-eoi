dd49402f65595227d4e0d9ff0c75715ac495a524
edusg83 <edusg83@gmail.com>
2022-11-14 19:47:55 +0100

    js

---
+++ js/ejercicio6.js
@@ -0,0 +1,41 @@
+var array1 = [1, 2, 3, 4];
+
+for (var x in array1)
+{
+    console.log(x);
+}
+
+for (var x in array1)
+{
+    console.log(array1[x]);
+}
+
+
+for(var i in array1){
+    switch(array1[i]){
+        case 1:
+            console.log("UNO")
+            break
+        case 2:
+            console.log("DOS")
+            break
+        case 3:
+            console.log("TRES")
+            break
+        case 4:
+            console.log("CUATRO")
+            break
+    }
+}
+
+array1.push(3);
+console.log(array1)
+
+
+//for(var i = 0; i<3; i++){
+//   array1.pop();
+//}
+array1.splice(-3);
+
+console.log(array1)
+
