825fe909916ab8f879c2c4eea0218ad87d413b76
NESTOR\nesto <nestorcostaorts@gmail.com>
2022-11-15 19:51:37 +0100

    Commit JS13

---
+++ js/ejercicio13.js
@@ -0,0 +1,8 @@
+function error() {
+    try {
+        aleeeeeert("nofunciona");
+    } catch(error) {
+        throw new EvalError(["Se ha producido un error",'ejercicio13.js','100']);
+    }
+};
+
