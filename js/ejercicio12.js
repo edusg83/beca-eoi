be0c64cb50d5d82ba3f687e71c47aa0c5d6cd888
NESTOR\nesto <nestorcostaorts@gmail.com>
2022-11-15 17:51:13 +0100

    Commit js12

---
+++ js/ejercicio12.js
@@ -0,0 +1,13 @@
+var nombre = "Sonia";
+
+var obj = {
+    nombre: "Pepito",
+    saludo: function(){
+        var saludo_fn = function() {
+            console.log("Hola " + this.obj.nombre);
+        };
+        saludo_fn();
+    }
+};
+
+obj.saludo();
