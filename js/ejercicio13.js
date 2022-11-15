42aa2bc24130332dceb480582e99d50851c240bc
Loren0723 <lorenelx@gmail.com>
2022-11-15 20:38:49 +0100

    commit

--- js/ejercicio13.js
+++ js/ejercicio13.js
@@ -1,7 +1,20 @@
-function test(){
-    try {
-        throw new EvalError(["Se ha producido un error", "ejercicio13.js", "100"]);
-      } catch (error) {
-        console.log(error);
-      }
+// function test(){
+//     try {
+//         throw new EvalError(["Se ha producido un error", "ejercicio13.js", "100"]);
+//       } catch (error) {
+//         console.log(error);
+//       }
+// }
+
+function sumaFecha() {
+    //creo la fecha con sus parametros
+    var year1 = document.getElementById("year").innerHTML;
+    var day1 = document.getElementById("day").innerHTML;
+    var month1 = document.getElementById("month").innerHTML;
+    var n1 = document.getElementById("n1").innerHTML;
+    var fecha = new Date(year1, day1, month1);
+    //hago la operacion
+    fecha.setMonth(fecha.getMonth()+ n1);
+    //lo inyecto en div en html
+    document.getElementById("div1").innerHTML = fecha;
 }
