fdacc5d80d25aea01e03c15c8114f2ee162383de
David López <DAVID@PC.(none)>
2022-11-15 19:45:52 +0100

    js13

--- js/ej13js.js
+++ js/ej13js.js
@@ -2,6 +2,6 @@ function error() {
     try {
         aleeeeeert("nofunciona");
     } catch(error) {
-        throw new EvalError(["Se ha producido un error",'ej13js.js','3']);
+        throw new EvalError(["Se ha producido un error",'ejercicio13.js','100']);
     }
 };
