b81d58d4d33d3519b384daa39cbf54335a337553
Manuel <manuel.molina.munoz99@gmail.com>
2022-11-15 16:30:09 +0100

    ej 9

---
+++ js/ejercicio9.js
@@ -0,0 +1,24 @@
+function suma (a, b){
+    return a+b
+};
+
+
+
+function mult (a, b){
+    return a*b
+};
+
+
+
+function operaciones(){
+    let n1 = Number(document.getElementById('numero1').innerHTML);
+    let n2 = Number(document.getElementById('numero2').innerHTML);
+    let sum = suma(n1, n2);
+    let multi = mult(n1, n2);
+    suma(sum, multi);
+}
+
+operaciones();
+
+
+
