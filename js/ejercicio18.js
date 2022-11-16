7b595c82e80a5d83d55277f8e62065e2e10a74da
Javi <mail>
2022-11-16 17:44:21 +0100

    c

---
+++ js/ejercicio18.js
@@ -0,0 +1,6 @@
+console.log(document.getElementsByTagName("header"));
+
+console.log(document.getElementById("miHeader"));
+
+console.log(document.querySelector("header"));
+console.log(document.querySelector("#miHeader"));
