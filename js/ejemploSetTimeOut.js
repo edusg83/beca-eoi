7fdfd62b01f2cdf626edfb83b66eee7e043b7544
Javi <mail>
2022-11-16 20:26:18 +0100

    c

---
+++ js/ejemploSetTimeOut.js
@@ -0,0 +1,25 @@
+/*
+var velocidad = 2000,
+i=0;
+
+miFuncion = function(){
+    console.log("Batman vuelve " + i);
+    i = i + 1;
+    if(i<10){
+        setTimeout(miFuncion,velocidad);
+    }
+};
+setTimeout(miFuncion,velocidad);
+*/
+
+var velocidad = 2000,
+i=0;
+
+miFuncion = function(){
+    console.log("Batman vuelve " + i);
+    i = i + 1;
+    if(i>9){
+        clearInterval(timer);
+    }
+};
+var timer = setInterval(miFuncion,velocidad);
