843f6e9d3eb1e3710a6b5087e085eb64240fa093
NESTOR\nesto <nestorcostaorts@gmail.com>
2022-11-16 16:23:05 +0100

    Commit js16

---
+++ js/ejercicio16.js
@@ -0,0 +1,13 @@
+
+
+console.log(navigator.language);
+
+console.log(navigator.cookieEnabled);
+
+console.log(navigator.appName);
+
+console.log(navigator.appVersion);
+
+console.log(navigator.product);
+
+console.log(navigator.userAgent);
