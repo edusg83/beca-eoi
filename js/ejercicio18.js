9c0100dd00cbc18128f56bee714d1f6d1ad2c989
Manuel <manuel.molina.munoz99@gmail.com>
2022-11-16 17:42:22 +0100

    ej17y18

---
+++ js/ejercicio18.js
@@ -0,0 +1,94 @@
+
+function porTag(){
+    var elementHeader1 = document.getElementsByTagName("header");
+    console.log(elementHeader1);
+}
+
+
+function porId(){
+    var elementHeader2 = document.getElementById("cabecera")
+    console.log(elementHeader2);
+}
+
+
+function porQuery(){
+    var elementHeader3 = document.querySelector("#cabecera");
+    console.log(elementHeader3);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+try{
+    eval = la"S;
+} catch (EvalError){
+    throw new EvalError("Error de sintaxis","fichero.js",100);
+}
+*/
