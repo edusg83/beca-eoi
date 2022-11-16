8c2191b3ca886dd2e886a1e74e6f41f92bb82c4f
Loren0723 <lorenelx@gmail.com>
2022-11-16 16:24:31 +0100

    commit

--- js/ejercicio15.js
+++ js/ejercicio15.js
@@ -1,5 +1,25 @@
-var nombre = prompt("Introduzca el nombre");
-console.log(nombre);
+// var nombre = prompt("Introduzca el nombre");
+// console.log(nombre);
 
-var continuar = confirm("¿Desea continuar?");
-console.log(continuar);
+// var continuar = confirm("¿Desea continuar?");
+// console.log(continuar);
+
+
+function sumar(n1, n2) {
+    return n1 + n2;
+}
+
+function multiplicar(n1, n2) {
+    return n1 * n2;
+}
+
+var n1 = prompt("Introduzca el primer valor");
+var n2 = prompt("Introduzca el segundoo valor");
+
+function operaciones() {
+    var suma = sumar(n1, n2);
+    var multiplicacion = multiplicar(n1, n2);
+    var resultado = sumar(suma, multiplicacion);
+    document.getElementById("resultado").innerHTML = resultado;
+    console.log(resultado);
+}
---
+++ js/ejercicio16.js
@@ -0,0 +1,6 @@
+console.log("Idioma = " + navigator.language);
+console.log("Cookies = " + navigator.cookieEnabled);
+console.log("Nombre de app = " + navigator.appName);
+console.log("Version de app = " + navigator.appVersion);
+console.log("Producto = " + navigator.product);
+console.log("Agente de usuario = " + navigator.userAgent);
