1a67f268db858ed87a84570ba215029687defb53
Manuel <manuel.molina.munoz99@gmail.com>
2022-11-16 16:26:15 +0100

    ej16

---
+++ js/ejercicio16.js
@@ -0,0 +1,86 @@
+console.log("Idioma: " + navigator.language);
+console.log("Cookies: " + navigator.cookieEnabled);
+console.log("AppName: " + navigator.appName);
+console.log("AppVersion: " + navigator.appVersion);
+console.log("Producto: " + navigator.product);
+console.log("Agente: " + navigator.userAgent);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+try{
+    eval = la"S;
+} catch (EvalError){
+    throw new EvalError("Error de sintaxis","fichero.js",100);
+}
+*/
