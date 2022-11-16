722561cfeafd71b42921ca261d3dca5939bc33fa
David López <DAVID@PC.(none)>
2022-11-16 17:41:20 +0100

    js18

---
+++ js/ej18js.js
@@ -0,0 +1,7 @@
+var getName = document.getElementsByTagName("header");
+var getId = document.getElementById("head");
+var getSel = document.querySelector("header");
+
+console.log(getName);
+console.log(getId);
+console.log(getSel);
