2b3d9d939194424e847ff3c93c20f69d2a4fdc34
NESTOR\nesto <nestorcostaorts@gmail.com>
2022-11-16 17:45:37 +0100

    Commit js18

---
+++ js/ejercicio18.js
@@ -0,0 +1,13 @@
+
+    var pElements = document.getElementsByTagName("header");
+    console.log(pElements);
+
+
+
+    var idElement = document.getElementById("header");
+    console.log(idElement);
+
+
+
+    var selecc = document.querySelector("#header");
+    console.log(selecc);
