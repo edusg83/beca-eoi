41941360f73479cbdc242bc637681e0c55937539
Javi <mail>
2022-11-16 16:22:49 +0100

    c

---
+++ js/ejercicio16.js
@@ -0,0 +1,5 @@
+console.log("Language: " + navigator.language);
+console.log("¿Cookies? " +  navigator.cookieEnabled);
+console.log("Nombre app: " + navigator.appName);
+console.log("Version app: " + navigator.appVersion);
+console.log("User agent: " + navigator.userAgent);
