acb150d9a0dcbffec2c76025a6a2ccbcc2158fae
edusg83 <edusg83@gmail.com>
2022-11-18 16:53:07 +0100

    ejercicio2 corregido

--- js/ejercicio2(apis).js
+++ js/ejercicio2(apis).js
@@ -1,21 +1,19 @@
 const request = new Request('https://j4jjw.mocklab.io/users');
 var datos;
+
 fetch(request)
 .then(response=>response.json())
 .then(data => {
     datos = data.arrayUsuarios;
-});
-
-
-let tabla = `<table id="dataTable">
-<thead>
-<tr>
-<th>Nombre</th>
-<th>Apellidos</th>
-<th>Direccion</th>
-</tr>
-</thead>
-<tbody>`;
+    let tabla = `<table id="dataTable">
+        <thead>
+        <tr>
+        <th>Nombre</th>
+        <th>Apellidos</th>
+        <th>Direccion</th>
+        </tr>
+        </thead>
+        <tbody>`;
 
 let finTabla=`</tbody>
 </table>`;
@@ -34,3 +32,7 @@ filas+=`<tr>
 
 tabla+=filas+finTabla;
 document.getElementById("resultados").innerHTML = tabla;
+});
+
+
+
