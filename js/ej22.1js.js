7a7b6f7423b0aade94a979f7ac5cc09dde8c8eb2
David López <DAVID@PC.(none)>
2022-11-18 15:22:53 +0100

    deberia funcionar

--- js/ej22.1js.js
+++ js/ej22.1js.js
@@ -23,17 +23,16 @@ function sumarmeses(suma,fechaActual) {
 function mensaje(fechaActual) {
 
     switch (fechaActual[1]) {
-        case 1,3,5,7,8,10,12: if (fechaActual[2] > 31) {fechaActual[2] = 31}
-        case 4,6,9,11: if (fechaActual[2] > 30) {fechaActual[2] = 30}
+        case 1,3,5,7,8,10,12: if (fechaActual[2] > 31) {fechaActual[2] = 31}; break;
+        case 4,6,9,11: if (fechaActual[2] > 30) {fechaActual[2] = 30}; break;
         case 2: if (esBisiesto(fechaActual[0])){
                     if (fechaActual[2] > 29) {fechaActual[2] = 29}
                 } else {
                     if (fechaActual[2] > 28) {fechaActual[2] = 28}
-                }
+                }; break;
     }
 
     if (fechaActual[1] < 10) {fechaActual[1] = '0'+String(fechaActual[1])};
-
 
     var updateFecha = fechaActual[0] + "/" + fechaActual[1] + "/" + fechaActual[2];
     document.getElementById("resultado").innerHTML=updateFecha;
