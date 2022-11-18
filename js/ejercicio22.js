6c273ae8636d7b27aa82e8f8ae010d854c42d2e7
Loren0723 <lorenelx@gmail.com>
2022-11-18 13:07:47 +0100

    commit

--- js/ejercicio22.js
+++ js/ejercicio22.js
@@ -1,51 +1,51 @@
 const form = document.getElementById("formulario1");
 
-// form.addEventListener("submit", function(elem) {
-//     elem.preventDefault();
-//     const formData = new FormData(form);
-//     var nombre = formData.get("nombre"),
-//         email = formData.get("email"),
-//         provincia = formData.get("provincia");
-    
-//     if(nombre.length > 0 && nombre.length <= 20 && nombre.includes("Antonio") === true && email != "" && provincia != "default"){
-//         parrafo.innerHTML = "Nombre: " + nombre + "</br>" + "Email: " + email + "</br>" + "Provincia: " + provincia;
-//     } else {
-//         parrafo.innerHTML = "Los datos del formulario no son correctos";
-//     }
-// });
-
-//OTRA MANERA DE HACERLO:
-
 form.addEventListener("submit", function(elem) {
     elem.preventDefault();
     const formData = new FormData(form);
     var nombre = formData.get("nombre"),
         email = formData.get("email"),
-        provincia = formData.get("provincia")
-        palabraInicial = nombre.startsWith("ANTONIO");
-        verificado = [false, false, false];
-        resultado = "";
-
-    if(nombre.length > 0 && nombre.length <= 20 && nombre.startsWith("Antonio") === true){
-        resultado += "Nombre: " + nombre;
-        verificado[0] = true;
-    }
-
-    if(email.length > 0) {
-        resultado += "<br>";
-        resultado += "Email: " + email;
-        resultado += "<br>";
-        verificado[1] = true;
-    }
-
-    if(provincia != "default") {
-        resultado += "Provincia: " + provincia;
-        verificado[2] = true;
-    }
-
-    if(verificado[0] === true && verificado[1] === true && verificado[2] === true) {
-        document.getElementById("parrafo").innerHTML = resultado;
+        provincia = formData.get("provincia");
+    
+    if(nombre.length > 0 && nombre.length <= 20 && nombre.startsWith("Antonio") === true && email != "" && provincia != "default"){
+        parrafo.innerHTML = "Nombre: " + nombre + "</br>" + "Email: " + email + "</br>" + "Provincia: " + provincia;
     } else {
-        document.getElementById("parrafo").innerHTML = "Los datos del formulario no son correctos";
+        parrafo.innerHTML = "Los datos del formulario no son correctos";
     }
 });
+
+//OTRA MANERA DE HACERLO:
+
+// form.addEventListener("submit", function(elem) {
+//     elem.preventDefault();
+//     const formData = new FormData(form);
+//     var nombre = formData.get("nombre"),
+//         email = formData.get("email"),
+//         provincia = formData.get("provincia")
+//         palabraInicial = nombre.startsWith("ANTONIO");
+//         verificado = [false, false, false];
+//         resultado = "";
+
+//     if(nombre.length > 0 && nombre.length <= 20 && nombre.startsWith("Antonio") === true){
+//         resultado += "Nombre: " + nombre;
+//         verificado[0] = true;
+//     }
+
+//     if(email.length > 0) {
+//         resultado += "<br>";
+//         resultado += "Email: " + email;
+//         resultado += "<br>";
+//         verificado[1] = true;
+//     }
+
+//     if(provincia != "default") {
+//         resultado += "Provincia: " + provincia;
+//         verificado[2] = true;
+//     }
+
+//     if(verificado[0] === true && verificado[1] === true && verificado[2] === true) {
+//         document.getElementById("parrafo").innerHTML = resultado;
+//     } else {
+//         document.getElementById("parrafo").innerHTML = "Los datos del formulario no son correctos";
+//     }
+// });
