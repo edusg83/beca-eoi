c1d0bf4fb50e1a01841a703c5536e6002e3dba4e
David López <DAVID@PC.(none)>
2022-11-22 18:51:54 +0100

    Update y Delete

--- EjEndPoints/EjendpointGET.js
+++ EjEndPoints/EjendpointGET.js
@@ -30,9 +30,9 @@ axios.get(request,{headers})
       <td>${item.name}</td>
       <td>${item.description}</td>
       <td>${item.code}</td>
-      <td><button type="button" class="btn btn-dark" data-toggle="modal" data-target="#modal">Update</button>
+      <td><button type="button" class="btn btn-secondary" onclick="verProducto(${item.id})" data-toggle="modal" data-target="#ModalUp">Update</button>
 
-      <button>Delete.</button></td>
+      <button type="button" class="btn btn-secondary" onclick="delProducto(${item.id})" data-toggle="modal" data-target="#ModalDel">Delete</button>
     </tr>`
     });
 
@@ -44,3 +44,57 @@ axios.get(request,{headers})
     document.getElementById("resultados").innerHTML = result;
     
 });
+
+
+
+function verProducto(param1) {
+  var url = request+"/"+param1
+
+  axios.get(url,{headers})
+.then((data) => {
+  
+  document.getElementById("producto-container").value = data.data.name;
+  document.getElementById("descripcion-container").value = data.data.description;
+  document.getElementById("codigo-container").value = data.data.code;
+
+  //formulario
+  var form = document.getElementById("formularioUpdate");
+
+  form.addEventListener("submit",function(element) {
+    element.preventDefault();
+    const envio = {
+      "id":data.data.id,
+      "name":document.getElementById("producto-container").value,
+      "description":document.getElementById("descripcion-container").value,
+      "code":document.getElementById("codigo-container").value
+    };
+    axios.put(url,envio,{headers})
+    .then(window.location.assign("EjendpointGET.html"));
+
+  });
+
+});
+}
+
+
+function delProducto(param1) {
+  var url = request+"/"+param1
+
+  axios.get(url,{headers})
+  .then((data)=> {
+    document.getElementById("producto-del").innerHTML = data.data.name;
+
+  var form = document.getElementById("formularioDelete");
+
+  form.addEventListener("submit",function(element) {
+    element.preventDefault();
+    const envio = {
+      "id":data.data.id
+    };
+    axios.delete(url,envio,{headers})
+    .then(window.location.assign("EjendpointGET.html"));
+
+  });
+});
+
+}
