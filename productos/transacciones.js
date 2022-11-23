2d330f69530b95fd5aa3f41bf1f6178d55c43c7e
Manuel <manuel.molina.munoz99@gmail.com>
2022-11-23 20:33:50 +0100

    m

--- productos/transacciones.js
+++ productos/transacciones.js
@@ -56,6 +56,65 @@ axios.get(url)
     });
 
 
+
+// GET ONE TRANSACTION
+const formulario = document.getElementById("formulario4");
+formulario.addEventListener("submit", function(element){
+    element.preventDefault();
+    const formData = new FormData(formulario);
+    
+    let ID = formData.get("ID");
+
+    axios.get('http://ligafalm.eu:28100/transactions/'+ID, {headers})
+        .then((respuesta)=>{
+            let transaccion = respuesta.data;
+            
+            if(transaccion.id===undefined){
+               document.getElementById("transaccionEncontrada").innerHTML = "Introduzca un ID";
+            } else {
+                encuentraTransaccion(transaccion);
+            }
+        })
+        .catch((error)=>{
+            document.getElementById("transaccionEncontrada").innerHTML = "Transaccion no existente",
+            console.log(error)
+    });    
+});
+// Funcion que muestra una transaccion
+function encuentraTransaccion(transaccion){
+    let tabla = `
+    <h1>Lista de transacciones</h1>
+            <table id="dataTable", class="tg">
+                <thead>
+                    <tr>
+                        <th class="th">Id</th>
+                        <th class="th">Código del producto</th>
+                        <th class="th">Total</th>
+                        <th class="th">Tipo</th>
+                        <th class="th">Objetivo</th> 
+                        <th class="th">Opciones</th>  
+                    </tr>
+                </thead>
+                <tbody>      
+                <tr>
+                <td class="tg-0lax">
+                    ${transaccion.id}
+                </td>
+                <td class="tg-0lax">${transaccion.productCode}</td>
+                <td class="tg-0lax", style="text-align:center;"> ${transaccion.total}</td>
+                <td class="tg-0lax", style="text-align:center;">${transaccion.type}</td>
+                <td class="tg-0lax", style="text-align:center;">${transaccion.goal}</td>
+                <td class="opciones"> 
+                    <a href="transaccion.html?id=${transaccion.id}")"><img src="editar.png", width=20px></a>
+                    <a onclick="borrarTransaccion(${transaccion.id})"><img src="borrar.png", width=20px></a>
+                </td>
+            </tr>
+        </tbody>
+    </table>`;
+    document.getElementById("transaccionEncontrada").innerHTML = tabla;
+}
+
+
 // DELETE ONE TRANSACTION
 function borrarTransaccion(id){
     let borrar = confirm("¿Desea borrar esta transacción?");
