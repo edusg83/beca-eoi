94aa00306d84293cfa64ee9fd0f8f8ad8d1cbb78
Ivan <ivancuarius@gmail.com>
2022-11-23 17:29:11 +0100

    Producto

--- Productos/api.js
+++ Productos/api.js
@@ -1,48 +1,64 @@
+
+// -- Inicio -----
+switch(window.location.href){
+    case "index":
+}
 if(window.location.href.indexOf("index")>0){
     mostrar();
-}else{
+}else if(window.location.href.indexOf("producto")>0){
     producto();
 }
+// ------------
 
-
-
-
-
-
+// -- Mostrar Lista -------
 function mostrar(){
     const headers ={
         'Content-Type':'application/json',
         'Acces-Control-Allow-Origin':'*'
     };
     
-    const urlUsers = "http://ligafalm.eu:28100/products";
+    const urlUsers = "http://ligafalm.eu:28100/products?page=0&size=100";
     
     axios.get(urlUsers,{headers})
     .then((respuestaProductos) => {
          let productos=respuestaProductos.data;
          let tabla=`
-         <div class="container">
-         <div class="row m-10">
-                        <div class="col-3" style="background-color: Grey;">Nombre</div>
-                        <div class="col-3" style="background-color: Grey;">Description</div>
-                        <div class="col-3" style="background-color: Grey;">Codigo</div> 
-                        <div class="col-1" style="background-color: Grey;">Opciones</div> `;
+         <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Codigo</th>
+                    <th scope="col">Opciones</th>
+                </tr>
+            </thead>
+            <tbody> `;
         
-         let finTabla=`</div>`;
+         let finTabla=`</tbody>
+         </table>`;
         
          let filastablas=``;
          productos.forEach(producto => {
             /*console.log("ID: "+producto.id+" Name: "+producto.name+" Descrpt: "+producto.description+" Code: "+producto.code);*/
             filastablas+=`
-                    <div class="col-3"  style="background-color: Lightgrey;"><a href="producto.html?id=${producto.id}">${producto.name}</a></div>
-                    <div class="col-3" style="background-color: Lightgrey;">${producto.description}</div>
-                    <div class="col-3" style="background-color: Lightgrey;">${producto.code}</div>
-                    <div class="col-1" onClick=borrar(${producto.id}) style="background-color: Lightgrey;">Borrar</div>`;
+            <tr>
+                <th scope="row">${producto.name}</th>
+                <td>${producto.description}</td>
+                <td>${producto.code}</td>
+                <td>
+                    <div>
+                        <img onClick=borrar(${producto.id}) src="img/eliminar.png" width="20" height="20">
+                        <img onClick=irPUt(${producto.id}) src="img/boligrafo.png" width="20" height="20">
+                    </div>
+                </td>
+            </tr>`;
          });
          tabla += filastablas+finTabla;
          document.getElementById("resultados").innerHTML=tabla;
         });
 }
+
+
 
 function mostrarID(id){
    
@@ -55,26 +71,41 @@ function mostrarID(id){
             
             axios.get(urlUsers,{headers})
             .then((respuestaProductos) => {
-                let productos=respuestaProductos.data;
+                let producto=respuestaProductos.data;
                 let tabla=`
-                <div class="container">
-                <div class="row m-10">
-                                <div class="col-4" style="background-color: Grey;">Nombre</div>
-                                <div class="col-4" style="background-color: Grey;">Description</div>
-                                <div class="col-4" style="background-color: Grey;">Codigo</div>
-                                <div class="col-4" style="background-color: Grey;">Opciones</div> `;
+                <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">Nombre</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Codigo</th>
+                    <th scope="col">Opciones</th>
+                </tr>
+            </thead>
+            <tbody> `;
                 
-                let finTabla=`</div> </div>`;
+            let finTabla=`</tbody>
+            </table>`;
                 
                 let filastablas=``;
                     /*console.log("ID: "+producto.id+" Name: "+producto.name+" Descrpt: "+producto.description+" Code: "+producto.code);*/
                     filastablas+=`
-                            <div class="col-2" style="background-color: Lightgrey;">${productos.name}</div>
-                            <div class="col-2" style="background-color: Lightgrey;">${productos.description}</div>
-                            <div class="col-2" style="background-color: Lightgrey;">${productos.code}</div>
-                            <div class="col-2" onClick=borrar(${productos.id})  style="background-color: Lightgrey;">Borrar</div>`;
-                
+                    <tr>
+                        <th scope="row">${producto.name}</th>
+                        <td>${producto.description}</td>
+                        <td>${producto.code}</td>
+                        <td>
+                            <div>
+                                <img onClick=borrar(${producto.id}) src="img/eliminar.png" width="20" height="20">
+                                <img onClick=irPUt(${producto.id}) src="img/boligrafo.png" width="20" height="20">
+                            </div>
+                        </td>
+                    </tr>`;
+                            
+                            
+                           
                 tabla += filastablas+finTabla;
+                console.log(tabla);
                 document.getElementById("resultados").innerHTML=tabla;
                 }).catch((error) =>{
                     let tabla=`
@@ -86,11 +117,31 @@ function mostrarID(id){
                             </div>
                             </div>`;
 
-                    document.getElementById("resultados").innerHTML=tabla;
+                    document.getElementById("resultadosfiltro").innerHTML=tabla;
                     window.alert("No se ha encontado");
                 });
 }
 
+function buscar(){
+    if(document.getElementById("buscador").value==""){
+        mostrar();
+    }else{
+        document.getElementById("resultados").innerHTML = null;
+        mostrarID(document.getElementById("buscador").value);
+    }
+    
+}
+
+function mostrarTodos(){
+    document.getElementById("resultados").innerHTML = "";
+    mostrar();
+}
+// -----------
+
+// -- Actualizar -------
+function irPUt(id){
+    window.location.href = "producto.html?id="+id;
+}
 function mostrarIDPro(id){
    
     const headers ={
@@ -107,39 +158,6 @@ function mostrarIDPro(id){
             document.getElementById("inDescription").value=productos.description;
             document.getElementById("inCodigo").value=productos.code;
             });
-}
-
-function buscar(){
-    if(document.getElementById("buscador").value==""){
-        mostrar();
-    }else{
-        document.getElementById("resultados").innerHTML = "";
-        mostrarID(document.getElementById("buscador").value);
-    }
-
-
-    console.log( );
-    
-}
-function borrar(id){
-    if (window.confirm("Estas seguro que deseas eliminar este producto?")) {
-        axios.delete("http://ligafalm.eu:28100/products/"+id)
-        .then(() => {
-            window.alert("Eliminado");
-            document.getElementById("resultados").innerHTML = "";
-            mostrar();
-        });
-      }
-     
-
-}
-function producto(){
-    var html = window.location.href; 
-  
-    var arrHtml = html.split("=");
-    idStr = arrHtml[1];
-
-    mostrarIDPro(idStr);
 }
 
 function PUTproducto(){
@@ -159,11 +177,28 @@ function PUTproducto(){
             });
             
 }
-function crear(){
-    window.location.href = "crear.html"
+// -----------------
+
+// -- Borrar ---------
+function borrar(id){
+    if (window.confirm("Estas seguro que deseas eliminar este producto?")) {
+        axios.delete("http://ligafalm.eu:28100/products/"+id)
+        .then(() => {
+            window.alert("Eliminado");
+            document.getElementById("resultados").innerHTML = "";
+            mostrar();
+        });
+      }
+     
+
 }
+
+// ---------------
+
+
+// -- Crear -------
 function Postproducto(){
-    axios.post("http://ligafalm.eu:28100/products",{
+    axios.post("http://ligafalm.eu:28100/products/",{
         name:document.getElementById("inNombre").value ,
         description:document.getElementById("inDescription").value,
         code:document.getElementById("inCodigo").value
@@ -174,4 +209,14 @@ function Postproducto(){
     
 
     
+}
+
+// ---------------------
+function producto(){
+    var html = window.location.href; 
+  
+    var arrHtml = html.split("=");
+    idStr = arrHtml[1];
+
+    mostrarIDPro(idStr);
 }
