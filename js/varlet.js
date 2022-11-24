10e8e0aa0d25e81db2d33599be6771eb522a6722
edusg83 <edusg83@gmail.com>
2022-11-24 17:06:41 +0100

    var&let

---
+++ js/varlet.js
@@ -0,0 +1,25 @@
+var saludar = "hey, hola";
+var tiempos = 4;
+
+if (tiempos > 3) {
+    var saludar = "dice Hola tambien"; 
+}
+
+console.log(saludar) // "dice Hola tambien"
+
+
+let saludar2 = "hey, hola";
+let tiempos2 = 4;
+
+if (tiempos > 3) {
+    let saludar2 = "dice Hola tambien"; 
+}
+
+console.log(saludar2) // "hey, hola"
+
+function hacerSaludo(){
+    let saludar2="hola, hola";
+    console.log(saludar2);
+}
+
+hacerSaludo();
