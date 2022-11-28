199659a133e1f777e9d9aea67e982845a7774970
NESTOR\nesto <nestorcostaorts@gmail.com>
2022-11-28 17:05:33 +0100

    Commit js

--- pruebas/built/app.js
+++ pruebas/built/app.js
@@ -1,3 +1,28 @@
 //let nombre:string='EEE';
-let nombre = 'Hola compañeros de la EOI';
+let nombre = 'Néstor';
+let edad = 27;
+let estudio = true;
+let formacion = ['RPA', 'Angular'];
+let direc;
+direc = ['Capitan Segarra', 18];
+var Cursos;
+(function (Cursos) {
+    Cursos[Cursos["Tecnico"] = 0] = "Tecnico";
+    Cursos[Cursos["Programador"] = 1] = "Programador";
+    Cursos[Cursos["Junior"] = 2] = "Junior";
+})(Cursos || (Cursos = {}));
+;
+let cursos = Cursos.Tecnico;
+let unknown;
+unknown = 'EXTRAÑO';
+let place = {
+    ubi: 'Rojales'
+};
 console.log(nombre);
+console.log(edad);
+console.log(estudio);
+console.log(formacion);
+console.log(direc[0]);
+console.log(cursos);
+console.log(unknown);
+console.log(place);
--- pruebas/src/app.ts
+++ pruebas/src/app.ts
@@ -1,5 +1,38 @@
 //let nombre:string='EEE';
 
-let nombre:string='Hola compañeros de la EOI';
+let nombre:string='Néstor';
+
+let edad:number=27;
+
+let estudio:boolean=true;
+
+let formacion:Array<string>= ['RPA', 'Angular'];
+
+let direc:[string,number];
+direc = ['Capitan Segarra',18];
+
+enum Cursos{Tecnico,Programador,Junior};
+let cursos:Cursos = Cursos.Tecnico;
+
+let unknown: any;
+unknown ='EXTRAÑO';
+
+let place: Object = {
+    ubi:'Rojales'
+};
 
 console.log(nombre);
+
+console.log(edad);
+
+console.log(estudio);
+
+console.log(formacion);
+
+console.log(direc[0]);
+
+console.log(cursos);
+
+console.log(unknown);
+
+console.log(place);
