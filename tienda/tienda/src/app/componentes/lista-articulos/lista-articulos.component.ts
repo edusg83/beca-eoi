9926900b849ec88a97580322fe5c11e091ddf0bc
NESTOR\nesto <nestorcostaorts@gmail.com>
2022-11-30 20:19:36 +0100

    Commit js

--- tienda/tienda/src/app/componentes/lista-articulos/lista-articulos.component.ts
+++ tienda/tienda/src/app/componentes/lista-articulos/lista-articulos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IArticulo } from 'src/app/interfaces/iarticulo';
 
 @Component({
   selector: 'app-lista-articulos',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./lista-articulos.component.css']
 })
 export class ListaArticulosComponent {
+
+  articulos:Array<IArticulo> = [
+    {
+    id:1,
+    name: 'Articulo1',
+    description: 'Descripcion Articulo1',
+    stock:5,
+    price:120
+    },
+
+    {
+      id:2,
+    name: 'Articulo2',
+    description: 'Descripcion Articulo2',
+    stock:5,
+    price:150
+    },
+
+    {
+      id:3,
+    name: 'Articulo3',
+    description: 'Descripcion Articulo3',
+    stock:0,
+    price:160
+    }
+  ]
+    
+  totalArticulos():number{
+    return this.articulos.reduce(
+      (acumulado,articulo)=> acumulado + articulo.stock,0
+    );
+  }
 
 }
